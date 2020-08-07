545f2455b1783472cd5f24fdbdf9b39366658ce4
KangChoi <kangchoi21@gmail.com>
2020-08-07 15:07:00 +0900

    dfs

--- graph/graph1/graphDataMatrix.h
+++ graph/graph1/graphDataMatrix.h
@@ -2,22 +2,31 @@
 #define GRAPH1_GRAPHDATAMATRIX_H
 
 #include <iostream>
-#include <vector>
 
-void getFixMatrix(std::vector<int *> intArr) {
-    int arr[] = {0, 1, 0, 0, 1, 0};
-    intArr.push_back(arr);
-    int arr2[] = {1, 0, 1, 1, 1, 0};
-    intArr.push_back(arr2);
-    int arr3[] = {0, 1, 0, 1, 0, 0};
-    intArr.push_back(arr3);
-    int arr4[] = {0, 1, 1, 0, 1, 1};
-    intArr.push_back(arr4);
-    int arr5[] = {1, 1, 0, 1, 0, 0};
-    intArr.push_back(arr5);
-    int arr6[] = {0, 0, 0, 1, 0, 0};
-    intArr.push_back(arr6);
+int** getFixedGraphMatrix() {
+    int matrix[6][6] = {
+            {0, 1, 0, 0, 1, 0},
+            {1, 0, 1, 1, 1, 0},
+            {0, 1, 0, 1, 0, 0},
+            {0, 1, 1, 0, 1, 1},
+            {1, 1, 0, 1, 0, 0},
+            {0, 0, 0, 1, 0, 0}
+    };
+    int graphDim = 6;
+
+    int **graphMatrix = new int *[graphDim];
+
+    for (int idx = 0; idx < graphDim; idx++) {
+        graphMatrix[idx] = new int[graphDim];
+
+        for (int idxsub = 0; idxsub < graphDim; idxsub++) {
+            graphMatrix[idx][idxsub] = matrix[idx][idxsub];
+        }
+    }
+
+    return graphMatrix;
 }
+
 
 int** getGraphMatrix(int graphDim) {
     int **graphMatrix = new int *[graphDim];
@@ -41,5 +50,16 @@ void printGraphMatrix(int **graphMatrix, int graphDim) {
         std::cout << std::endl;
     }
 }
+
+int** executeCustomMatrix(int graphDim) {
+    int **graphMatrix;
+
+    graphMatrix = getGraphMatrix(graphDim);
+    printGraphMatrix(graphMatrix, graphDim);
+
+    return graphMatrix;
+}
+
+
 
 #endif //GRAPH1_GRAPHDATAMATRIX_H
--- graph/graph1/main.cpp
+++ graph/graph1/main.cpp
@@ -1,18 +1,42 @@
 #include <iostream>
-#include <stack>
 #include <vector>
-#include <array>
 
-#include "graphDataMatrix.h"
+void dfs(int start, std::vector<int> graph[], bool check[]) {
+    check[start] = true;
+    std::cout << start;
+
+    for (int idx = 0; idx < graph[start].size(); idx++) {
+        int next = graph[start][idx];
+
+        if (check[next] == false){
+            dfs(next, graph, check);
+        }
+    }
+}
 
 int main() {
-    int graphDim;
-    std::cout << "Square Matrix Graph Dimension : ", std::cin >> graphDim;
+    int nodes, edges;
+    std::cin >> nodes >> edges;
 
-    int **graphMatrix;
-    graphMatrix = getGraphMatrix(graphDim);
-    printGraphMatrix(graphMatrix, graphDim);
+    std::vector<int> graph[nodes];
+
+    bool check[nodes];
+    std::fill(check, check + nodes, false);
+
+    for (int idx = 0; idx < edges; idx++) {
+        int node1, node2;
+        std::cin >> node1 >> node2;
+
+        graph[node1].push_back(node2);
+        graph[node2].push_back(node1);
+    }
+
+    for (int idx = 0; idx < nodes; idx++) {
+        std::sort(graph[idx].begin(), graph[idx].end());
+    }
+
+    int start = 0;
+    dfs(start, graph, check);
 
     return 0;
 }
-
