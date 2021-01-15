ee6d922d1b204823c22bc64a6977b50b9db28b05
kangchoi <kangchoi21@gmail.com>
2021-01-15 20:30:32 +0900

    tree

---
+++ CodingInterviewBook/Tree/44_longest_univalue.py
@@ -0,0 +1,71 @@
+from CodingInterviewBook.Tree.common import tree_node, make_tree
+
+from collections import deque
+
+# fail need to modify
+def check_longest_univalue_path(root):
+    max_value = [0]
+
+    def dfs(node):
+        if node == None:
+            return 0
+
+        left_value = dfs(node.left)
+        right_value = dfs(node.right)
+
+        if node.left and node.value == node.left.value:
+            left_value += 1
+        else:
+            left_value = 0
+        if node.right and node.value == node.right.value:
+            right_value += 1
+        else:
+            right_value = 0
+
+        if left_value >= right_value:
+            value = left_value
+        else:
+            value = right_value
+
+        if max_value[0] < value:
+            max_value[0] = value
+
+        # if node.value == node.left.value:
+        #     left_value = dfs(node.left) + 1
+        #     if max_value[0] < left_value:
+        #         max_value[0] = left_value
+        # else:
+        #     dfs(node.left)
+        #     left_value = 0
+        #
+        # if node.value == node.right.value:
+        #     right_value = dfs(node.right) + 1
+        #     if max_value[0] < right_value:
+        #         max_value[0] = right_value
+        # else:
+        #     dfs(node.right)
+        #     right_value = 0
+        #
+        # if left_value >= right_value:
+        #     value = left_value
+        # else:
+        #     value = right_value
+
+        return value
+
+    dfs(root)
+    return max_value[0]
+
+
+if __name__ == "__main__":
+    input_1 = [5, 4, 5, 1, 1, 5]
+    input_2 = [1, 4, 5, 4, 4, 5]
+
+    input_1_tree = make_tree(input_1)
+    input_2_tree = make_tree(input_2)
+
+    path_1 = check_longest_univalue_path(input_1_tree)
+    path_2 = check_longest_univalue_path(input_2_tree)
+
+    print(path_1)
+    print(path_2)
--- CodingInterviewBook/Tree/common.py
+++ CodingInterviewBook/Tree/common.py
@@ -24,3 +24,21 @@ class tree_node:
     def right(self, node):
         self._right = node
 
+
+def make_tree(tree_list):
+    tree_class_list = list()
+
+    for value in tree_list:
+        node = tree_node(value)
+        tree_class_list.append(node)
+
+    for idx, node in enumerate(tree_class_list):
+        left_index = 2 * idx + 1
+        right_index = 2 * idx + 2
+
+        if left_index < len(tree_list):
+            node.left = tree_class_list[left_index]
+        if right_index < len(tree_list):
+            node.right = tree_class_list[right_index]
+
+    return tree_class_list[0]
